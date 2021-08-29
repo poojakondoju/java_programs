99067a94ec93d4b356b0957822bbd5cc1b84e225
poojakondoju <kondojupoojitha612@gmail.com>
2021-08-29 19:37:33 +0530

    Print the year is a Leap Year or not.

--- Leapyear.java
+++ Leapyear.java
@@ -1,20 +1,18 @@
 package javaprograms;
-   
+
 import java.util.Scanner;
 
-	public class Leapyear {
-		 public static void main(String[] args){
-		      int year;
-		      System.out.println("Enter an Year :: ");
-		      Scanner sc = new Scanner(System.in);
-		           year = sc.nextInt();
+public class Leapyear {
+	public static void main(String[] args) {
+		int year;
+		System.out.println("Enter an Year :: ");
+		Scanner sc = new Scanner(System.in);
+		year = sc.nextInt();
 
-		      if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
-		         System.out.println(" Its a leap year");
-		      else
-		         System.out.println(" Its not a leap year");
-		   }
-
+		if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+			System.out.println(" Its a leap year");
+		else
+			System.out.println(" Its not a leap year");
 	}
 
-
+}
