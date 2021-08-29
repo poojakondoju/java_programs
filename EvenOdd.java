e39843a8d747aba72c08fbb9e9131dda8c9a0b18
poojakondoju <kondojupoojitha612@gmail.com>
2021-08-29 19:21:42 +0530

    evenodd

--- EvenOdd.java
+++ EvenOdd.java
@@ -3,18 +3,13 @@ package javaprograms;
 public class EvenOdd {
 	public static void main(String[] args)
     {
-        
-        int num = 5;
-  
-        if (num % 2 == 0) {
-  
-        
-            System.out.println(" Number is Even");
+         int num = 5;
+         if (num % 2 == 0) {
+         System.out.println(" Number is Even");
         }
-        else {
-  
-            
-            System.out.println("Number is odd");
+        else 
+        {
+         System.out.println("Number is odd");
         }   
     }
 
